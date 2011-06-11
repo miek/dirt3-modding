b7ee2d3370b4c3ecb9b0f04349fb2d252ec67831
Petar Tasev <tasevp@yahoo.com>
2011-06-11 05:12:56 -0700

    Added textures tab along with previews and dds writing function.

--- tools/pssgmanager/pssgmanager/Main.cs
+++ tools/pssgmanager/pssgmanager/Main.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Xml;
 using System.Text;
 using System.Windows.Forms;
+using FreeImageAPI;
 
 namespace PSSGManager {
 	public partial class Main : Form {
@@ -25,6 +26,7 @@ namespace PSSGManager {
 				CPSSGFile f = new CPSSGFile(sr.BaseStream);
 				pssgFile = f;
 				treeView.Nodes.Add(createTreeViewNode(f.rootNode));
+                createTreeViewTexturesList(f.rootNode);
 			} else {
 
 			}
@@ -78,5 +80,81 @@ namespace PSSGManager {
 			}
 			writer.WriteEndElement();
 		}
+
+        private void createTreeViewTexturesList(CNode node)
+        {
+            if (node.name == "TEXTURE")
+            {
+                TreeNode treeNode = new TreeNode();
+                treeNode.Text = node.attributes["id"].value;
+                treeNode.Tag = node;
+                treeViewTextures.Nodes.Add(treeNode);
+            }
+            if (node.subNodes != null)
+            {
+                foreach (CNode subNode in node.subNodes)
+                {
+                    createTreeViewTexturesList(subNode);
+                }
+            }
+        }
+
+        private void treeViewTextures_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            int height = 0; int width = 0;
+            pictureBoxTextures.Dock = DockStyle.Fill;
+            height = pictureBoxTextures.Height;
+            width = pictureBoxTextures.Width;
+            writeDDS(Application.StartupPath + "\\temp.dds");
+            FREE_IMAGE_FORMAT format = FREE_IMAGE_FORMAT.FIF_DDS;
+            System.Drawing.Bitmap image = FreeImage.LoadBitmap(Application.StartupPath + "\\temp.dds", FREE_IMAGE_LOAD_FLAGS.DEFAULT, ref format);
+            if (image.Height <= height && image.Width <= width)
+            {
+                pictureBoxTextures.Dock = DockStyle.None;
+                pictureBoxTextures.Width = image.Width;
+                pictureBoxTextures.Height = image.Height;
+            }
+            pictureBoxTextures.Image = image;
+        }
+
+        private void writeDDS(string ddsPath)
+        {
+            byte[] ddh;
+            CNode node = ((CNode)treeViewTextures.SelectedNode.Tag);
+            using (BinaryReader b = new BinaryReader(File.Open(Application.StartupPath + "\\dxt.ddh", FileMode.Open)))
+            {
+                ddh = b.ReadBytes(128);
+            }
+            using (BinaryWriter b = new BinaryWriter(File.Open(ddsPath, FileMode.Create)))
+            {
+                b.Write(ddh);
+                b.Write(node.subNodes[0].subNodes[0].data);
+            }
+            using (Stream outStream = File.Open(ddsPath, FileMode.Open))
+            {
+                outStream.Seek(12, SeekOrigin.Begin);
+                // Change Height and Width
+                outStream.Write(BitConverter.GetBytes((int)node.attributes["height"].data), 0, 4);
+                outStream.Write(BitConverter.GetBytes((int)node.attributes["width"].data), 0, 4);
+                // Change size
+                switch ((string)node.attributes["texelFormat"].data)
+                {
+                    case "dxt1":
+                        outStream.Write(BitConverter.GetBytes(((int)node.attributes["width"].data) * ((int)node.attributes["height"].data) / 2), 0, 4);
+                        break;
+                    default:
+                        outStream.Write(BitConverter.GetBytes(((int)node.attributes["width"].data) * ((int)node.attributes["height"].data)), 0, 4);
+                        break;
+                }
+                // Skip 4
+                outStream.Seek(4, SeekOrigin.Current);
+                // Change numberMipMapLevels + 1
+                outStream.Write(BitConverter.GetBytes((int)node.attributes["numberMipMapLevels"].data + 1), 0, 4);
+                // Skip 52
+                outStream.Seek(52, SeekOrigin.Current);
+                // Change Format
+                outStream.Write(Encoding.UTF8.GetBytes(((string)node.attributes["texelFormat"].data).ToUpper()), 0, 4);
+            }
+        }
 	}
 }
